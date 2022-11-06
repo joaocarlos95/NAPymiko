97ff81713c818e7ea7bc4e91da3ca04648f79ec4
joaocarlos95 <joaocarlos95@gmail.com>
2022-11-06 17:13:47 +0000

    Delete unnecessary files

--- networkautomation/common/main.py
+++ networkautomation/common/main.py
@@ -12,108 +12,108 @@ from networkautomation.global_variables import global_variables as gv
 
 
 
-def run_serial_connection(device_type, username, password, enable_secret):
+# def run_serial_connection(device_type, username, password, enable_secret):
 
-    serial_port = get_serial_port()
-    serial_connection = ConnectHandler(
-        device_type = "{}_serial".format(device_type),
-        username = username,
-        password = password,
-        fast_cli = False,
-        conn_timeout = 30,
-        serial_settings = {
-            "baudrate": serial.Serial.BAUDRATES[12],
-            "bytesize": serial.EIGHTBITS,
-            "parity": serial.PARITY_NONE,
-            "stopbits": serial.STOPBITS_ONE,
-            "port": serial_port,
-        },
-    )
-    if not serial_connection.check_enable_mode():
-        serial_connection.secret = enable_secret
-        serial_connection.enable()
-    return serial_connection
-
-
-def run_ssh_connection(device_type, device_ip, username, password, enable_secret):
-
-    ssh_connection = ConnectHandler(
-        device_type = device_type,
-        ip = device_ip, 
-        username = username,
-        password = password,
-        banner_timeout = 10,    # for issue: Error reading SSH protocol banner
-    )
-    if not ssh_connection.check_enable_mode():
-        ssh_connection.secret = enable_secret
-        ssh_connection.enable()
-    return ssh_connection
+#     serial_port = get_serial_port()
+#     serial_connection = ConnectHandler(
+#         device_type = "{}_serial".format(device_type),
+#         username = username,
+#         password = password,
+#         fast_cli = False,
+#         conn_timeout = 30,
+#         serial_settings = {
+#             "baudrate": serial.Serial.BAUDRATES[12],
+#             "bytesize": serial.EIGHTBITS,
+#             "parity": serial.PARITY_NONE,
+#             "stopbits": serial.STOPBITS_ONE,
+#             "port": serial_port,
+#         },
+#     )
+#     if not serial_connection.check_enable_mode():
+#         serial_connection.secret = enable_secret
+#         serial_connection.enable()
+#     return serial_connection
 
 
-def run_telnet_connection(device_type, device_ip, username, password, enable_secret):
+# def run_ssh_connection(device_type, device_ip, username, password, enable_secret):
 
-    telnet_connection = ConnectHandler(
-        device_type = "{}_telnet".format(device_type),
-        ip = device_ip, 
-        username = username,
-        password = password,
-        banner_timeout = 10,    # for issue: Error reading SSH protocol banner
-    )
-    if not telnet_connection.check_enable_mode():
-        telnet_connection.secret = enable_secret
-        telnet_connection.enable()
-    return telnet_connection
+#     ssh_connection = ConnectHandler(
+#         device_type = device_type,
+#         ip = device_ip, 
+#         username = username,
+#         password = password,
+#         banner_timeout = 10,    # for issue: Error reading SSH protocol banner
+#     )
+#     if not ssh_connection.check_enable_mode():
+#         ssh_connection.secret = enable_secret
+#         ssh_connection.enable()
+#     return ssh_connection
 
 
-def run_command(vendor, platform, device_ip, username, password, enable_secret, command, textfsm):
-    ''' Connect to a given device and run the requested command
-    Parameters: device IP, credentials (user and password), and command to run
-    Return: output of the command '''
+# def run_telnet_connection(device_type, device_ip, username, password, enable_secret):
 
-    try:
+#     telnet_connection = ConnectHandler(
+#         device_type = "{}_telnet".format(device_type),
+#         ip = device_ip, 
+#         username = username,
+#         password = password,
+#         banner_timeout = 10,    # for issue: Error reading SSH protocol banner
+#     )
+#     if not telnet_connection.check_enable_mode():
+#         telnet_connection.secret = enable_secret
+#         telnet_connection.enable()
+#     return telnet_connection
+
+
+# def run_command(vendor, platform, device_ip, username, password, enable_secret, command, textfsm):
+#     ''' Connect to a given device and run the requested command
+#     Parameters: device IP, credentials (user and password), and command to run
+#     Return: output of the command '''
+
+#     try:
         
-        device_type = "{}_{}".format(vendor, platform)
-        if gv.get_connection_method() == "serial":
-            connection = run_serial_connection(device_type, username, password, enable_secret)
-            device_ip = "serial connection"
-        if gv.get_connection_method() == "telnet":
-            connection = run_telnet_connection(device_type, device_ip, username, password, enable_secret)
-        else:
-            connection = run_ssh_connection(device_type, device_ip, username, password, enable_secret)
+#         device_type = "{}_{}".format(vendor, platform)
+#         if gv.get_connection_method() == "serial":
+#             connection = run_serial_connection(device_type, username, password, enable_secret)
+#             device_ip = "serial connection"
+#         if gv.get_connection_method() == "telnet":
+#             connection = run_telnet_connection(device_type, device_ip, username, password, enable_secret)
+#         else:
+#             connection = run_ssh_connection(device_type, device_ip, username, password, enable_secret)
 
-        hostname = connection.find_prompt()[:-1]
-        device_id = "{} ({})".format(hostname, device_ip)
-        print("[>] Logging into the device: {}".format(device_id))
+#         hostname = connection.find_prompt()[:-1]
+#         device_id = "{} ({})".format(hostname, device_ip)
+#         print("[>] Logging into the device: {}".format(device_id))
         
-        print("[>] Running command: {}".format(command))
-        raw_command_output = connection.send_command(command, delay_factor=2, read_timeout=600).lstrip()
+#         print("[>] Running command: {}".format(command))
+#         raw_command_output = connection.send_command(command, delay_factor=2, read_timeout=600).lstrip()
 
-        if gv.get_raw_output():
-            filename = "{} - {}".format(device_id, command)
-            write_info_to_txt("Raw Data", filename, "{}{}".format("{}# {}\n".format(hostname, command), raw_command_output))
+#         if gv.get_raw_output():
+#             filename = "{} - {}".format(device_id, command)
+#             write_info_to_txt("Raw Data", filename, "{}{}".format("{}# {}\n".format(hostname, command), raw_command_output))
 
-        if not textfsm:
-            return hostname, raw_command_output        
-        else:
-            if device_type == "cisco_wlc":
-                device_type = "cisco_wlc_ssh"
-            command_output = get_structured_data(raw_command_output, platform=device_type, command=command)
+#         if not textfsm:
+#             return hostname, raw_command_output        
+#         else:
+#             if device_type == "cisco_wlc":
+#                 device_type = "cisco_wlc_ssh"
+#             command_output = get_structured_data(raw_command_output, platform=device_type, command=command)
         
-        if type(command_output) != list:
-            raise Exception("[!] Error parsing the output")
+#         if type(command_output) != list:
+#             raise Exception("[!] Error parsing the output")
 
-        if tv.command_exists(device_type, command):
-            command_output = tv.remove_textfsm_variables(command_output, device_type, command)
+#         if tv.command_exists(device_type, command):
+#             command_output = tv.remove_textfsm_variables(command_output, device_type, command)
 
-    except Exception as exception:
-        if "TCP connection to device failed" in str(exception):
-            raise Exception("[!] Error connecting to the device")
-        elif "[!] Error parsing the output" in str(exception):
-            raise Exception("[!] Error parsing the output")
-        else:
-            raise Exception("[!] Error (main function)]", exception)
+#     except Exception as exception:
+#         if "TCP connection to device failed" in str(exception):
+#             raise Exception("[!] Error connecting to the device")
+#         elif "[!] Error parsing the output" in str(exception):
+#             raise Exception("[!] Error parsing the output")
+#         else:
+#             raise Exception("[!] Error (main function)]", exception)
             
-    return hostname, command_output
+#     return hostname, command_output
    
 
 def set_configuration(vendor, platform, device_ip, username, password, configurations):
