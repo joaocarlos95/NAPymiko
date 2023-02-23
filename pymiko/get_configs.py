29953b1e1c33d98d4ec18a051d9a54dd4bc8713b
joaocarlos95 <joaocarlos95@gmail.com>
2023-02-23 15:55:04 +0000

    Refactor get_configs.py

--- pymiko/get_configs.py
+++ pymiko/get_configs.py
@@ -2,73 +2,56 @@
 
 import json
 import os
-import pprint
 import sys
 import time
-
-from classes import Client
 from multiprocessing import Manager, Process
+from classes import Client
 
 
-def raise_exception(exception): sys.exit("[!] {}".format(exception))
+def raise_exception(exception):
+    sys.exit(f"[!] {exception}")
 
 
 if __name__ == '__main__':
-
     st = time.time()
 
     with open(os.path.join(os.path.dirname(__file__), './get_configs.txt'), 'r', encoding='utf-8') as file:     
-
         get_configs_info = []
-        line = file.readline()
-        while line != '':
+        keepass_db = keepass_pwd = None
 
+        for line in file:
             if 'Root Directory:' in line:
-                next_line = file.readline()
-                root_dir = next_line.split('>')[1].strip() if '>' in next_line and len(set(next_line)) > 2 else raise_exception('Root directory not specified')
+                line = file.readline()
+                root_dir = line.split('>')[1].strip() if '>' in line and len(set(line)) > 2 else raise_exception('Root directory not specified')
 
             elif 'Client Name:' in line:
-                next_line = file.readline()
-                client_name = next_line.split('>')[1].strip() if '>' in next_line and len(set(next_line)) > 2 else raise_exception('Client name not specified')
+                line = file.readline()
+                client_name = line.split('>')[1].strip() if '>' in line and len(set(line)) > 2 else raise_exception('Client name not specified')
             
             elif 'Keepass Database:' in line:
-                # IF NO KEEPASS DATABASE IS SPECIFIED, AN EXCEPTION IS RAISED AND SHOULDN'T. MUST PASS AND IF CANNOT FIND CREDENTIALS, THEN AN EXCEPTION MUST BE RAISED
-                next_line = file.readline()
-                keepass_db = next_line.split('>')[1].strip() if '>' in next_line and len(set(next_line)) > 2 and '.kdbx' in next_line else raise_exception('Keepass database not specified')
-                # keepass_pwd = input("[>] Keepass password: ")
-                keepass_pwd = 'HOX265huq#'
+                line = file.readline()
+                if '>' in line and len(set(line)) > 2 and '.kdbx' in line:
+                    keepass_db = line.split('>')[1].strip()
+                    keepass_pwd = input('[>] Keepass password: ')
                 
             elif '[x]' in line or '[X]' in line:
                 info_requested = line.split('] ')[1].rstrip()
                 get_configs_info.append(info_requested)
             
-            line = file.readline()
-
     client = Client(root_dir, client_name, keepass_db=keepass_db, keepass_pwd=keepass_pwd)
     manager = Manager()
     report = manager.list()
 
-    process_list = []
-    for devive in client.device_list:
-        # Append in a list the process of acquiring information for the device
-        process = Process(target=devive.run_get_configs, args=(get_configs_info, report))
-        process_list.append(process)
-
-    # Initiate the process of acquiring configurations for each device
-    for process in process_list:
-        process.start()
-    # Wait for each process to finish
-    for process in process_list:
-        process.join()
-
-    # Generage reports for this script
+    process_list = [Process(target=device.run_get_configs, args=(get_configs_info, report)) for device in client.device_list]
+    [process.start() for process in process_list]
+    [process.join() for process in process_list]
+    
     client.report = report
     client.generate_config_report()
     client.generate_config_parsed()
 
-    # Save all script output in a json file
     with open(f"{client.dir}/outputfiles/script_output.json", "w") as outfile:
-        outfile.write(json.dumps(list(report), indent=2))
+        json.dump(list(report), outfile, indent=2)
 
     if 'Network Diagram' in get_configs_info:
         client.generate_diagram()
